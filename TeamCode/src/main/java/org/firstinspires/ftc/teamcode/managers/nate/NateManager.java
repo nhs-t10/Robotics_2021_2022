4a5be9d551a82c3177a628d2364f6e8041bffd35
shaashwat <shaash317@gmail.com>
2021-11-02 19:56:39 -0400

    NateManager

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/nate/NateManager.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.managers.nate;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.auxilary.PaulMath;
+import org.firstinspires.ftc.teamcode.managers.manipulation.ManipulationManager;
+import org.firstinspires.ftc.teamcode.managers.movement.MovementManager;
+
+public class NateManager {
+    private boolean input;
+    private boolean found;
+
+    public void foldOut(){
+
+    }
+    public void homing(){
+        ManipulationManager manipulationManager = null;
+        if (input) {
+            boolean found=true;
+            //DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+        }
+        if (found) {
+            manipulationManager.setMotorPower("outake",-1);
+        } else {
+            manipulationManager.setMotorPower("Outake", PaulMath.proportionalPID(0,0));
+        }
+    }
+}
