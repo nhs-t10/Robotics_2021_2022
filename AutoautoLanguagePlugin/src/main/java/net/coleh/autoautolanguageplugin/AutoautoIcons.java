a15e36720509fb31e3f5efd519616eadf4ad08ae
coleh2 <coleh@coleh.net>
2021-11-28 05:02:46 -0500

    Re-remove Autoauto language plugin sources to prevent unduely long builds

--- AutoautoLanguagePlugin/src/main/java/net/coleh/autoautolanguageplugin/AutoautoIcons.java
+++
@@ -1,9 +0,0 @@
-package net.coleh.autoautolanguageplugin;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.Icon;
-
-public class AutoautoIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/autoauto-16.png");
-}
