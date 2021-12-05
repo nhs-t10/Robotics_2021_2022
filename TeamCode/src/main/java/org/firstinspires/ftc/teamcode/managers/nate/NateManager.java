044044956fcdd8fd2379762eb2eab63d245afd39
coleh2 <coleh@coleh.net>
2021-12-05 11:14:02 -0500

    Document NateManager

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/nate/NateManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/nate/NateManager.java
@@ -22,10 +22,24 @@ public class NateManager extends FeatureManager {
         this.hands = hands;
     }
 
-    public void foldOutLift(){
-
+    //TODO: test and remove untested warning.
+    /**
+     * Make the claw open or close
+     * <h2>Warning: This method has not been tested in the wild. Use it, but don't rely fully until it's been tested.</h2>
+     *
+     * @param open If {@true}, open the claw; otherwise, close it.
+     */
+    public void setClawOpen(boolean open) {
+        //by setting the `clawState` variable to the opposite and then calling toggleClawOpen(), we can cut down on code reuse.
+        //toggleClawOpen() will reverse it back to the value the user asked for.
+        clawState = !open;
+        toggleClawOpen();
     }
 
+    /**
+     * Toggle whether the robot's claw is open.
+     * If the claw is closed, it'll become open. If it's open, it'll become closed.
+     */
     public void toggleClawOpen(){
         if (clawState == false) {
             hands.setServoPosition("nateClaw", 1.0);
@@ -40,6 +54,24 @@ public class NateManager extends FeatureManager {
         }
     }
 
+    //TODO: test and remove untested warning.
+    /**
+     * Get the current claw status
+     * <h2>Warning: This method has not been tested in the wild. Use it, but don't rely fully until it's been tested.</h2>
+     *
+     * @return {@code true} if the claw is open, {@code false} if the claw is closed.
+     */
+    public boolean getClawStatus() {
+        return clawState;
+    }
+
+    /**
+     * Stop any asynchronous movement of the claw lift immediately
+     * @see #positionOne()
+     * @see #positionTwo()
+     * @see #positionThree()
+     * @see #positionHome()
+     */
     public void emergencyStop(){
 //        currentPos = hands.getMotorPosition("ClawMotor");
 //        currentPosition = (int) Math.round(currentPos);
@@ -47,6 +79,31 @@ public class NateManager extends FeatureManager {
         hands.cancelEncodedMovement("ClawMotor");
     }
 
+    /**
+     * Checks that the claw lift <u>isn't</u> moving asynchronously.
+     *
+     * @see #positionOne()
+     * @see #positionTwo()
+     * @see #positionThree()
+     * @see #positionHome()
+     *
+     * @return {@code false} if the claw lift has any asynchronous movement; {@code true} otherwise
+     */
+    public boolean liftMovementFinished() {
+        return !hands.hasEncodedMovement("ClawMotor");
+    }
+
+    /**
+     * <p>Move the claw's lift to position 1, the highest position.</p>
+     *
+     * <p>Note that this is an <u>asynchronous</u> method, meaning that the method will complete before the action does.
+     * Because of this, something like {@code positionOne(); toggleClawOpen()} will open the claw <u>right away</u>.
+     * </p>
+     * <p>
+     * If you want autonomous to have synchronous-ish behaviour, use the {@link #liftMovementFinished clawMovementFinished()} method, like so:
+     * <pre><code> positionOne(), when(clawMovementFinished()) next; </code></pre>
+     * </p>
+     */
     public void positionOne(){
         if (!found){
             position = -1490;
@@ -59,6 +116,17 @@ public class NateManager extends FeatureManager {
         hands.encodeMoveToPosition("ClawMotor", position, 0.75);
     }
 
+    /**
+     * <p>Move the claw's lift to position 2, the middle position.</p>
+     *
+     * <p>Note that this is an <u>asynchronous</u> method, meaning that the method will complete before the action does.
+     * Because of this, something like {@code positionTwo(); toggleClawOpen()} will open the claw <u>right away</u>.
+     * </p>
+     * <p>
+     * If you want autonomous to have synchronous-ish behaviour, use the {@link #liftMovementFinished clawMovementFinished()} method, like so:
+     * <pre><code> positionTwo(), when(clawMovementFinished()) next; </code></pre>
+     * </p>
+     */
     public void positionTwo(){
         if(!found) {
             position = -3024;
@@ -69,6 +137,17 @@ public class NateManager extends FeatureManager {
         hands.encodeMoveToPosition("ClawMotor", position, 0.75);
     }
 
+    /**
+     * <p>Move the claw's lift to position 3, the lowest position.</p>
+     *
+     * <p>Note that this is an <u>asynchronous</u> method, meaning that the method will complete before the action does.
+     * Because of this, something like {@code positionThree(); toggleClawOpen()} will open the claw <u>right away</u>.
+     * </p>
+     * <p>
+     * If you want autonomous to have synchronous-ish behaviour, use the {@link #liftMovementFinished clawMovementFinished()} method, like so:
+     * <pre><code> positionThree(), when(clawMovementFinished()) next; </code></pre>
+     * </p>
+     */
     public void positionThree(){
         if(!found) {
             position = -4679;
@@ -79,6 +158,17 @@ public class NateManager extends FeatureManager {
         hands.encodeMoveToPosition("ClawMotor", position, 0.75);
     }
 
+    /**
+     * <p>Move the claw's lift back to the home position inside the chassis in order to intake objects.</p>
+     *
+     * <p>Note that this is an <u>asynchronous</u> method, meaning that the method will complete before the action does.
+     * Because of this, something like {@code positionHome(); toggleClawOpen()} will open the claw <u>right away</u>.
+     * </p>
+     * <p>
+     * If you want autonomous to have synchronous-ish behaviour, use the {@link #liftMovementFinished clawMovementFinished()} method, like so:
+     * <pre><code> positionHome(), when(clawMovementFinished()) next; </code></pre>
+     * </p>
+     */
     public void positionHome(){
         if(!found){
             position = 2537;
@@ -88,6 +178,16 @@ public class NateManager extends FeatureManager {
 
         hands.encodeMoveToPosition("ClawMotor", position, 0.75);
     }
+
+    /**
+     * <p>Move the claw back to the {@link #positionHome() home position} by scanning the sensors on the lift.</p>
+     *
+     * <p>This is an asynchronous method, meaning that the method will complete before the action does. Unfortunately, the {@link #liftMovementFinished()}
+     * method won't work here. Try using time instead :(</p>
+     *
+     * <h2>TODO: <i><u>THIS DOES NOT WORK.</u></i> We should make it work.</h2>
+     * <p>To do that, we need a sensor connected in the code.</p>
+     */
 
     public void homing(){
         if (input) {
