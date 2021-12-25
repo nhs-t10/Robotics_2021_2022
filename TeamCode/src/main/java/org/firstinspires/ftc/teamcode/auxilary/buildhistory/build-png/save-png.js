b8a1a5d8995f109475e00db161b9b69f497f0804
chloe h <coleh@coleh.net>
2021-12-25 12:50:50 -0500

    Fix a minor oops that had build images generating outside of their directory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/buildhistory/build-png/save-png.js
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/buildhistory/build-png/save-png.js
@@ -10,6 +10,6 @@ var assetsDir = path.join(mainDirectory, "assets");
 if(!fs.existsSync(path.join(assetsDir, "buildimgs"))) fs.mkdirSync(path.join(assetsDir, "buildimgs"), { recursive: true });
 
 module.exports = function(number, pngBuffer) {
-    fs.writeFileSync(path.join(assetsDir, "buildimgs" + number + ".png"), pngBuffer); //SAFE
+    fs.writeFileSync(path.join(assetsDir, "buildimgs/" + number + ".png"), pngBuffer); //SAFE
     return "buildimgs/" + number + ".png";
 }
