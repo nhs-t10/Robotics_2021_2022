2f271bb434e3d45601cd5381633f7a59427bd7f2
chlohal <coleh@coleh.net>
2022-01-31 15:07:33 -0500

    Wait the time doesn't work on this sdk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/RobotTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/RobotTime.java
@@ -1,24 +1,16 @@
 package org.firstinspires.ftc.teamcode.auxilary;
 
-import android.os.Build;
-
-import java.time.Clock;
-import java.time.ZoneId;
-
-import androidx.annotation.RequiresApi;
-
 /**
  * A class that lets us play around with the time. This is <i>very</i> fast, and lets us get precision over System.currentTimeMillis, where it's possible.
  */
 public class RobotTime {
-    public static Clock clock = Clock.systemUTC();
 
     /**
      * The same as System.currentTimeMillis, but with greater precision if the device allows it
      * @see System#currentTimeMillis()
      */
     public static long currentTimeMillis() {
-        return clock.millis();
+        return System.currentTimeMillis();
     }
 
     /**
@@ -26,6 +18,6 @@ public class RobotTime {
      * @see System#nanoTime()
      */
     public static long nanoTime() {
-        return clock.instant().getNano();
+        return System.nanoTime();
     }
 }
