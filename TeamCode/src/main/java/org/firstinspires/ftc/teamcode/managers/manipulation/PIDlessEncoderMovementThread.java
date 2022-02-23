41373565f3f65a9319493b3efb059fb39604c9d5
chlohal <coleh@coleh.net>
2022-02-23 15:55:42 -0500

    Active position seeking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/manipulation/PIDlessEncoderMovementThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/manipulation/PIDlessEncoderMovementThread.java
@@ -11,12 +11,12 @@ import org.firstinspires.ftc.teamcode.managers.feature.FeatureManager;
 public class PIDlessEncoderMovementThread extends Thread {
     DcMotor[] motors;
     private int[] targets;
-    private boolean[] shouldMove;
+    private boolean[] isMoving;
+    private boolean[] hasTarget;
     private double[] powerCoefs;
     //checks
     private float[] direction;
     private float[] lastDeltas;
-    private boolean[] zeroed;
 
     AutotuningPIDController[] controllers;
 
@@ -27,15 +27,13 @@ public class PIDlessEncoderMovementThread extends Thread {
     public PIDlessEncoderMovementThread(DcMotor[] motors) {
         this.motors = motors;
         this.targets = new int[motors.length];
-        this.shouldMove = new boolean[motors.length];
+        this.hasTarget = new boolean[motors.length];
         this.powerCoefs = new double[motors.length];
         
         this.lastDeltas = new float[motors.length];
 
         this.direction = new float[motors.length];
         for (int i = 0; i < motors.length; i++) direction[i] = 1;
-
-        this.zeroed = new boolean[motors.length];
 
         this.controllers = new AutotuningPIDController[motors.length];
 
@@ -49,7 +47,7 @@ public class PIDlessEncoderMovementThread extends Thread {
         try {
             while(FeatureManager.isOpModeRunning) {
                 for(int i = 0; i < motors.length; i++) {
-                    if(shouldMove[i]) {
+                    if(hasTarget[i]) {
                         if(this.controllers[i] == null) this.controllers[i] = new AutotuningPIDController(ManipulationManager.ENCODER_TICK_VALUE_TOLERANCE);
 
                         if(motors[i].getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER) motors[i].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -68,9 +66,7 @@ public class PIDlessEncoderMovementThread extends Thread {
 
                         motors[i].setPower(Range.clip(power, -0.3,0.3));
 
-                        if(controllers[i].isStable(currentPos)) {
-                            shouldMove[i] = false;
-                        }
+                        isMoving[i] = !controllers[i].isStable(currentPos);
 
                         lastDeltas[i] = delta;
                     }
@@ -84,11 +80,11 @@ public class PIDlessEncoderMovementThread extends Thread {
     }
 
     public boolean isMoving(int i) {
-        return shouldMove[i];
+        return isMoving[i];
     }
 
     public void move(int i, int target, double power) {
-        shouldMove[i] = true;
+        hasTarget[i] = true;
         powerCoefs[i] = power;
         targets[i] = target;
     }
@@ -97,6 +93,6 @@ public class PIDlessEncoderMovementThread extends Thread {
         return targets[i];
     }
     public void cancelMovement(int index) {
-        shouldMove[index] = false;
+        hasTarget[index] = false;
     }
 }
