265752933d31b8ae3d307af4bd0f286515061fbb
chlohal <coleh@coleh.net>
2022-03-04 12:56:54 -0500

    Don't warn when units are indiscriminate, but equivalent (e.g. inch and international inch)

--- TeamCode/src/main/scripts/unit-conversion/index.js
+++ TeamCode/src/main/scripts/unit-conversion/index.js
@@ -18,8 +18,24 @@ function getUnitForAbbreviation(abb) {
     var key = unitData.index[abb];
     
     if(!key) return undefined;
-    else if(key.length > 1) return key;
+    else if(key.length > 1) return discriminateToUnique(key);
     else return unitData.data[key[0]];
+}
+
+function discriminateToUnique(unitNames) {
+    var uniq = [];
+    var used = {}
+    
+    for(var i = 0; i < unitNames.length; i++) {
+        var record = unitData.data[unitNames[i]];
+        if(!used[record.conversionFactors]) {
+            uniq.push(unitNames[i]);
+            used[record.conversionFactors] = true;
+        }
+    }
+    console.log(uniq);
+    if(uniq.length == 1) return unitData.data[uniq[0]];
+    else return uniq;
 }
 
 function loadData() {
